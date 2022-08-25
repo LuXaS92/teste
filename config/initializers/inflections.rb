cbab8294cb2e27506d8194531930da86e4c66f9a
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-08-25 18:00:58 -0300

    novo_projeto

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -9,7 +9,9 @@
 #   inflect.irregular 'person', 'people'
 #   inflect.uncountable %w( fish sheep )
 # end
-
+ActiveSupport::Inflector.inflections(:en) do |inflect|
+  inflect.plural 'Comentário', 'Comentários'
+end
 # These inflection rules are supported but not enabled by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
