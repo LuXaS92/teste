ee2542a7c7b5103939170bc4b2f79420338c3ba4
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-08-26 11:36:09 -0300

    atualizaçao 3

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.create!(comments_params)
+    redirect_to comment.post, notice: 'O seu comentario foi enviado.'
+  end
+
+  private 
+
+  def comments_params
+    params.require(:comment).permit(:post_id, :author, :body)
+  end
+end
